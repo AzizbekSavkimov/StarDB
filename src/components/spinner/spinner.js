27b5c83fcc7eb6f990b832394f43aef497e6770b
asavkimov <azizbeksavkimov@mail.ru>
2020-07-19 13:41:55 +0500

    StarDB

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-double-ring-et5b29r7oki">
+            <div class="ldio-l1bxj9kwcb">
+                <div></div>
+                <div></div>
+                <div><div></div></div>
+                <div><div></div></div>
+            </div>
+        </div>
+    );
+}
+
+export default Spinner;

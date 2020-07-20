a1e4410851857ff375502e85d405cb56e95d124a
asavkimov <azizbeksavkimov@mail.ru>
2020-07-20 11:15:09 +0500

    StarDb

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
@@ -6,23 +6,42 @@ import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 
 import './app.css';
+import ErrorIndicator from '../error-indicator';
 
-const App = () => {
-  return (
-    <div className="container-fluid">
-      <Header />
-      <RandomPlanet />
+export default class App extends Component {
+  state = {
+    selectedPerson: 5,
+    hasError: false
+  };
 
-      <div className="row mb2 block">
-        <div className="col-md-6">
-          <ItemList />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
+  onPersonSelected = (id) => {
+    this.setState({
+      selectedPerson: id
+    });
+  }
+
+  componentDidCatch() {
+    this.setState({hasError: true})
+  }
+
+  render() {
+    if(this.state.hasError) {
+      return <ErrorIndicator />
+    }
+    return (
+      <div className="container-fluid">
+        <Header />
+        <RandomPlanet />
+  
+        <div className="row mb2 block">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onPersonSelected} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson}/>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
-
-export default App;
